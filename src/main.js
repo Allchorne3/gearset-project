34bd1d5a1bbc6d0229bdbe6576f1dbc2b5040c54
Allchorne3 <38890461+Allchorne3@users.noreply.github.com>
2022-05-26 13:26:54 +0100

    Adjusted some gsap animation times

--- src/main.js
+++ src/main.js
@@ -68,40 +68,36 @@ $(document).ready(function(){
 
     // DevOps Solved
     let tl = gsap.timeline({
-        scrollTrigger: {trigger: '#devops-solved', start: "10% bottom"}
-      })
-      
-      tl.from("#devops-solved .container--small h2", { y: 20, opacity: 0, duration: 0.7})
-        .from("#devops-solved .container--small p", { y: 20, opacity: 0, duration: 0.7}, "-=0.3")
-        .from("#devops-solved .devops-benefits .devops-benefit", { y: 20, opacity: 0, duration: 1, stagger: 0.3}, "-=0.1")
+      scrollTrigger: {trigger: '#devops-solved', start: "10% bottom"}
+    })  
+    tl.from("#devops-solved .container--small h2", { y: 20, opacity: 0, duration: 0.7})
+      .from("#devops-solved .container--small p", { y: 20, opacity: 0, duration: 0.7}, "-=0.3")
+      .from("#devops-solved .devops-benefits .devops-benefit", { y: 20, opacity: 0, duration: 1, stagger: 0.3}, "-=0.1")
       
       // About Us
-      let tl1 = gsap.timeline({
-        scrollTrigger: {trigger: '#about-us', start: "center bottom"}
-      })
-      
-    //  tl1.to(".about-us_content-left figure", {xPercent: -100, ease:Power2.out, duration: 1.5})
-        .from(".about-us_content-right h2", { y: 30, opacity: 0, duration: 1}, "-=1.3")
-        .from(".about-us_content-right p", { y: 30, opacity: 0, duration: 1}, "-=1")
+    let tl1 = gsap.timeline({
+      scrollTrigger: {trigger: '#about-us', start: "center bottom"}
+    })
+    tl1.from(".about-us_content-left figure", {y: 30, opacity: 0, duration: 1})
+       .from(".about-us_content-right h2", { y: 30, opacity: 0, duration: 1}, "-=1.3")
+       .from(".about-us_content-right p", { y: 30, opacity: 0, duration: 1}, "-=0.7")
           
       // DevOps Standards
-      let tl2 = gsap.timeline({
+    let tl2 = gsap.timeline({
         scrollTrigger: {trigger: '#devops-standards', start: "10% bottom"}
-      })
-  
-      tl2.from("#devops-standards .container--small h2", { y: 20, opacity: 0, duration: 0.7})
-         .from("#devops-standards .container--small p", { y: 20, opacity: 0, duration: 0.7}, "-=0.3")
-         .from("#devops-standards .container--small a", { y: 40, opacity: 0, duration: 0.7}, "-=0.5")
-         .from("#devops-standards .slick-slider .slick-slide", { y: 20, opacity: 0, duration: 0.7, stagger: 0.25}, "-=1.2")
+    })
+    tl2.from("#devops-standards .container--small h2", { y: 20, opacity: 0, duration: 0.7})
+       .from("#devops-standards .container--small p", { y: 20, opacity: 0, duration: 0.7}, "-=0.3")
+       .from("#devops-standards .container--small a", { y: 40, opacity: 0, duration: 0.7}, "-=0.5")
+       .from("#devops-standards .slick-slider .slick-slide", { y: 20, opacity: 0, duration: 0.7, stagger: 0.1}, "-=1.2")
       
       // Learn More
-      let tl3 = gsap.timeline({
-        scrollTrigger: {trigger: '#learn-more', start: "30% bottom"}
-      })
-  
-      tl3.from("#learn-more h2", {opacity: 0, duration: 0.7})
-         .from("#learn-more p", {opacity: 0, duration: 0.7}, "-=0.3")
-         .from("#learn-more a", {opacity: 0, duration: 0.7}, "-=0.3")
+    let tl3 = gsap.timeline({
+      scrollTrigger: {trigger: '#learn-more', start: "30% bottom"}
+    })
+    tl3.from("#learn-more h2", {opacity: 0, duration: 0.7})
+       .from("#learn-more p", {opacity: 0, duration: 0.7}, "-=0.3")
+       .from("#learn-more a", {opacity: 0, duration: 0.7}, "-=0.3")
 
     // Change year in the footer dynamically
     $('.year').text(new Date().getFullYear());
